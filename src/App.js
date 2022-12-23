70269b17c8fba2e51f037f0ad7348d4a6c2c9875
Shailesh Bisht <shailesh.bisht@habilelabs.io>
2022-12-24 00:55:10 +0530

    setting line and bar graph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
 import "./App.css";
 import { arr } from "../src/data/data";
+import ReactECharts from "echarts-for-react";
+import { useState } from "react";
 
 function App() {
-  //creating new array of oobject
-  const averages = [
-    ...arr.reduce(
-      (map, { Alcohol, "Malic Acid": malicAcid }) =>
-        map.set(Alcohol, [...(map.get(Alcohol) || []), malicAcid]),
-      new Map()
-    ),
-  ].map(([Alcohol, malicAcid]) => ({
-    Alcohol,
-    malicAcid: malicAcid.reduce((sum, val) => sum + val, 0) / malicAcid.length,
-  }));
+  const [chartType, setChartType] = useState("bar");
 
-  console.log(averages);
+  //creating new array of oobject
+  // const averages = [
+  //   ...arr.reduce(
+  //     (map, { Alcohol, "Malic Acid": malicAcid }) =>
+  //       map.set(Alcohol, [...(map.get(Alcohol) || []), malicAcid]),
+  //     new Map()
+  //   ),
+  // ].map(([Alcohol, malicAcid]) => ({
+  //   Alcohol,
+  //   malicAcid: malicAcid.reduce((sum, val) => sum + val, 0) / malicAcid.length,
+  // }));
+
+  // console.log(averages);
+
+  // get x-axis and y-axis data
+
+  // let result = {};
+  // averages.forEach((average) => {
+  //   const { Alcohol, malicAcid } = average;
+  //   result[`Alochol${Alcohol}`] = malicAcid;
+  // });
+  // console.log(Object.keys(result));
+  // console.log(Object.values(result));
+
+  //
+
+  // alternative code
+
+  let malicAcidSum = {};
+  let lengthMalicAcid = {};
+  let resultNew = {};
+  arr.forEach((wine) => {
+    const { Alcohol } = wine;
+    const Malic = wine["Malic Acid"];
+    let key = `Alcohol-${Alcohol}`;
+    if (key in malicAcidSum) {
+      let value = malicAcidSum[key];
+      malicAcidSum[key] = value + Malic;
+      lengthMalicAcid[key] += 1;
+    } else {
+      malicAcidSum[key] = Malic;
+      lengthMalicAcid[key] = 1;
+    }
+  });
+
+  Object.keys(malicAcidSum).forEach((key) => {
+    resultNew[key] = malicAcidSum[key] / lengthMalicAcid[key];
+  });
+
+  let xaxis = Object.keys(resultNew);
+  let yaxis = Object.values(resultNew);
+
+  console.log(chartType);
+
+  //
+
+  const options = {
+    grid: { top: 8, right: 8, bottom: 24, left: 36 },
+    xAxis: {
+      type: "category",
+      data: xaxis,
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: yaxis,
+        type: chartType,
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: "axis",
+    },
+  };
 
   return (
     <div className="App">
-      <h1>Data Visualization Task</h1>
+      <h1 className="app__title">Data Visualization Task</h1>
+
+      <div className="app__chart-type">
+        <p>select the chart type</p>
+        <select
+          value={chartType}
+          onChange={(e) => setChartType(e.target.value)}
+        >
+          <option value="line">line</option>
+          <option value="bar">bar</option>
+        </select>
+      </div>
+
+      <div className="app__graph">
+        <ReactECharts option={options} />
+      </div>
     </div>
   );
 }
